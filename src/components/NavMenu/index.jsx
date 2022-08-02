546f51c857c9b5dca6b40537b89543d7e45bec6e
Antonio Wellington <antoniowellington542@gmail.com>
2022-08-02 15:02:20 -0300

    refactor: sale page

--- src/components/NavMenu/index.jsx
+++ src/components/NavMenu/index.jsx
@@ -18,7 +18,7 @@ const NavMenu = () =>{
                         {open == true ? <AiOutlineClose/> : <VscMenu /> }
                     </span>
                 </div>
-                    <ul className={`md:flex md:items-center z-[-1] md:z-auto md:static absolute bg-white w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-4 md:opacity-100 ${open == true ? 'top-[100px]' : 'top-[-400px]'} transition-all ease-in duration-1000`}>
+                    <ul className={`md:flex md:items-center md:justify-center z-[1] md:z-auto md:static absolute bg-white w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-4 md:opacity-100 ${open == true ? 'top-[50px] h-full' : 'h-[0px] top-[-500px]'} transition-all ease-in duration-1000`}>
                         <li className="mx-4 my-6 md:my-0 text-xl hover:text-green-400 duration-500 cursor-pointer">
                             <Link href="/dashboard">HOME</Link>
                         </li>
