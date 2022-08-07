d68fc816c03c79d425cae052a79ffe36a9d52cd9
antoniowellington542 <antoniowellington542@gmail.com>
2022-08-07 16:33:32 -0300

    refactor: dashboard page

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -36,8 +36,8 @@ const Dashboard = ()=>{
                                                     <div className="flex justify-between">
                                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 text-blue-400"
                                                             fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                                stroke-width="2"
+                                                            <path strokeLinecap="round" strokeLinejoin="round"
+                                                                strokeWidth="2"
                                                                 d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
                                                         </svg>
                                                         <div
@@ -59,8 +59,8 @@ const Dashboard = ()=>{
                                                     <div className="flex justify-between">
                                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 text-yellow-400"
                                                             fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                                stroke-width="2"
+                                                            <path strokeLinecap="round" strokeLinejoin="round"
+                                                                strokeWidth="2"
                                                                 d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
                                                         </svg>
                                                         <div
@@ -82,11 +82,11 @@ const Dashboard = ()=>{
                                                     <div className="flex justify-between">
                                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 text-pink-600"
                                                             fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                                stroke-width="2"
+                                                            <path strokeLinecap="round" strokeLinejoin="round"
+                                                                strokeWidth="2"
                                                                 d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z" />
-                                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                                stroke-width="2"
+                                                            <path strokeLinecap="round" strokeLinejoin="round"
+                                                                strokeWidth="2"
                                                                 d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z" />
                                                         </svg>
                                                         <div
@@ -108,8 +108,8 @@ const Dashboard = ()=>{
                                                     <div className="flex justify-between">
                                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 text-green-400"
                                                             fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                                stroke-width="2"
+                                                            <path strokeLinecap="round" strokeLinejoin="round"
+                                                                strokeWidth="2"
                                                                 d="M7 12l3-3 3 3 4-4M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z" />
                                                         </svg>
                                                         <div
